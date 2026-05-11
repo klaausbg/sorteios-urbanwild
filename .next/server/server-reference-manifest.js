b67926f91645876581386a22a5dad6d00e0e793a
Klaus Boeger <klausboeger@hotmail.com>
2026-05-11 16:48:14 -0300

    removendo pasta .next

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U19tKMkFADfAqBM0M1Kx4l3G+Lv0e8uZjF1XaNoxC+E=\"\n}"
--- .next/static/chunks/src_app_como-funciona_page_tsx_f33d95b0._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_590dce30._.js"
-],
-    source: "dynamic"
-});
